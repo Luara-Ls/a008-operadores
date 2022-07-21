e26d594aae3b5746abdddcb57d0473b9937f7832
Luara <luaraap7@gmail.com>
2022-07-21 18:46:06 -0300

    exercicio2/finalizado

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,1 +1,12 @@
 
+const numeroPar = prompt("Insira aqui um número par")
+console.log (numeroPar)
+
+console.log(typeof numeroPar)
+
+let saldoAtual = numeroPar % 2 
+console.log(saldoAtual)
+
+//C - O resto da divisão é igual a 0
+//D - Com número impar o resto aparece
+console.log(+numeroPar)
